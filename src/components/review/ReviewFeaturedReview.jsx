8cc9dd2739ee414237afdd1f91123c0b4c79b450
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-10-27 11:58:29 -0500

    featured review artwork and text formatted

--- src/components/review/ReviewFeaturedReview.jsx
+++ src/components/review/ReviewFeaturedReview.jsx
@@ -1,10 +1,14 @@
 const ReviewFeaturedReview = ({ albumPicture, reviewText }) => {
   return (
-    <div>
-      <img src={albumPicture} alt="album artwork" />
-      {reviewText.split("\n").map((textLine) => (
-        <p>{textLine}</p>
-      ))}
+    <div className="review-featured-review-div card">
+      <div className="album-picture-div">
+        <img className="album-picture" src={albumPicture} alt="album artwork" />
+      </div>
+      <div className="review-text-div">
+        {reviewText.split("\n").map((textLine) => (
+          <p className="review-text">{textLine}</p>
+        ))}
+      </div>
     </div>
   );
 };
