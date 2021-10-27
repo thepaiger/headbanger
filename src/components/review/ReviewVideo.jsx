d126194bac65a0bee98800b071d77ff5e0055cc3
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-10-27 10:36:17 -0500

    video resized

--- src/components/review/ReviewVideo.jsx
+++ src/components/review/ReviewVideo.jsx
@@ -2,8 +2,14 @@ import ReactPlayer from "react-player/youtube";
 
 const ReviewVideo = ({ musicVideo }) => {
   return (
-    <div className="video">
-      <ReactPlayer url={musicVideo} />
+    <div className="video-div">
+      <ReactPlayer
+        className="react-player"
+        url={musicVideo}
+        alt="music video"
+        // width="100%"
+        // height="100%"
+      />
     </div>
   );
 };
